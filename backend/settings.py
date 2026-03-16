9efbb2f49a13c358e58e0c768f2caa463bf9bb18
boresa <issahsalim233@gmail.com>
2026-03-16 11:57:24 +0000

    fixed communication

--- backend/settings.py
+++ backend/settings.py
@@ -80,8 +80,7 @@ TEMPLATES = [
 
 CORS_ALLOWED_ORIGINS = [
     "https://profelvisasarebediako.netlify.app/",
-    "http://localhost:5173", 
-    "http://127.0.0.1:5173",
+     
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
